71d031c13d911a7974ce0b6ba1c48c662ead1b5b
TrungHieu29 <ahieu2910@gmail.com>
2026-04-27 11:23:04 +0700

    chore: setup project structure and database configuration

---
+++ src/main/java/com/trunghieu/fashioncommerce/fashion_commerce_backend/FashionCommerceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.trunghieu.fashioncommerce.fashion_commerce_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionCommerceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashionCommerceBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trunghieu/fashioncommerce/fashion_commerce_backend/FashionCommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trunghieu.fashioncommerce.fashion_commerce_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionCommerceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
